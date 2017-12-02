4ee08d3fc9a4dff7585fdd93b3d03f1b51fa8c94
gruan <gruan@asnum.com>
2017-12-02 15:49:41 +0800

    DB

--- OM.AppServer/OMHandler.cs
+++ OM.AppServer/OMHandler.cs
@@ -1,15 +1,19 @@
-﻿using OM.Api;
+﻿using CNB.Common;
+using OM.Api;
 using OM.AppServer.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace OM.AppServer
 {
     public static class OMHandler
     {
+
+        private static Biz.CDRBiz Biz = new AppServer.Biz.CDRBiz();
 
         public static void Init()
         {
@@ -39,6 +43,27 @@ namespace OM.AppServer
         private static void ApiClient_OnReceiveCDR(object sender, OMCDREventArgs e)
         {
             OMHubHelper.Send(e.Data);
+            Task.Run(async () =>
+            {
+                await Biz.AddCDR(new Entity.CDR()
+                {
+                    CallID = e.Data.CallID,
+                    Duration = e.Data.Duration,
+                    From = e.Data.From,
+                    ID = e.Data.ID,
+                    OuterID = e.Data.Outer?.ID.ToInt(0) ?? 0,
+                    RecCodec = e.Data.RecCodec,
+                    Recording = e.Data.Recording,
+                    RecoredOn = DateTime.Now,
+                    Route = (byte)e.Data.Route,
+                    TimeEnd = e.Data.TimeEnd,
+                    TimeStar = e.Data.TimeStar,
+                    To = e.Data.To,
+                    TrunkNumber = e.Data.TrunkNumber,
+                    Type = (byte)e.Data.Type,
+                    VisitorID = e.Data.Visitor?.ID.ToInt(0) ?? 0
+                });
+            }).Wait();
         }
     }
 }
